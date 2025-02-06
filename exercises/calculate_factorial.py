b0c9d896129e22381686af95886a212d296aa79c
Esther Levin <estylevin0@gmail.com>
2025-02-06 18:48:58 -0500

    completed python exercise finding the factorial of a non negative number

--- exercises/calculate_factorial.py
+++ exercises/calculate_factorial.py
@@ -1,16 +1,24 @@
 # implement factorial_recursive method
 def factorial_recursive(number):
-    pass
+    if number == 0 or number == 1:
+        return 1
+    return number * factorial_recursive(number - 1)
 
 
 def main():
-    print("Factorial Computation Using Recursion")
-    number = int(input("Enter a non-negative integer: "))
-
     # handle negative input
+    try:
+        print("Factorial Computation Using Recursion")
+        number = int(input("Enter a non-negative integer: "))
 
+        while number < 0:
+            number = int(input("Must enter a non-negative integer. Please re-enter: "))
+
+        factorial = factorial_recursive(number)
+        print("The factorial of " + str(number) + " is: " + str(factorial))
     # Call factorial_recursive method
-
+    except ValueError:
+        print("Invalid input. Non integer value entered.")
 
 if __name__ == "__main__":
     main()

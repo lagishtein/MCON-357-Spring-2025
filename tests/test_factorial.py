9eb9539aee58cc82d20957901dce602d213bb6a7
Miriam Berg <miriambrg04@gmail.com>
2025-02-22 22:38:19 -0500

    Removed try from recursion limit test

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -29,14 +29,10 @@ class TestFactorial(unittest.TestCase):
     def test_factorial_recursion_limit(self):
         #get python's recursion depth limit
         large_number = sys.getrecursionlimit() // 2 #divide by 2 so that the recursive_factorial function does not hit the limit and cause error
-        try:
-            result_recursive = factorial_recursive(large_number)
-            result_iterative = factorial_iterative(large_number)
-            self.assertEqual(result_recursive, result_iterative)
-        #if recursion error is hit, test is forced to fail and prints fail message
-        except RecursionError:
-            self.fail("factorial_recursive() exceeded recursion limit")
-
+        result_recursive = factorial_recursive(large_number)
+        result_iterative = factorial_iterative(large_number)
+        self.assertEqual(result_recursive, result_iterative)
+        
     def test_factorial_above_recursion_limit(self):
         large_number = sys.getrecursionlimit() // 2 + 1
         # Compare against built in math factorial function

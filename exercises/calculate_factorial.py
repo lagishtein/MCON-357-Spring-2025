2fcad4510b24c9766a66c78fdeb794a3ae3f909a
bdrillic2 <bdrillic2@student.touro.edu>
2025-02-27 10:43:17 -0500

    completed python code to calculate factorial

--- exercises/calculate_factorial.py
+++ exercises/calculate_factorial.py
@@ -1,6 +1,11 @@
 # implement factorial_recursive method
 def factorial_recursive(number):
-    pass
+    # Base Case: If the number is 0 or 1, return 1
+    if number == 0 or number == 1:
+        return 1
+
+    # Recursive Case: number * factorial of (number - 1)
+    return number * factorial_recursive(number - 1)
 
 
 def main():
@@ -8,9 +13,13 @@ def main():
     number = int(input("Enter a non-negative integer: "))
 
     # handle negative input
+    if number < 0:
+        print("Error: You must enter a non-negative integer.")
+        return
 
     # Call factorial_recursive method
-
+    result = factorial_recursive(number)
+    print(f"The factorial of {number} is: {result}")
 
 if __name__ == "__main__":
     main()
